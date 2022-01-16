947db9c321f0a4a01cbb2c9be32fa2f3f033a403
Andrew Sheridan <andrewasheridan@gmail.com>
2022-01-16 14:46:45 -0500

    fix(longest_palindromic_substring): now 10x faster
    needs explanatory comments still

--- src/leetcode/longest_palindomic_substring/answer.py
+++ src/leetcode/longest_palindomic_substring/answer.py
@@ -4,28 +4,33 @@
 def longest_palindromic_substring(s: str) -> str:
     """Get the longest palindromic substring.
 
-    Runtime: 8392 ms, faster than 7.41% of Python3 online submissions for Longest Palindromic Substring.
-    Memory Usage: 82.6 MB, less than 5.32% of Python3 online submissions for Longest Palindromic Substring.
+    Runtime: 120 ms, faster than 97.84% of Python3 online submissions for Longest Palindromic Substring.
+    Memory Usage: 14.3 MB, less than 81.11% of Python3 online submissions for Longest Palindromic Substring.
 
-    lol heckaslow
     """
 
     def is_palindrome(_str: str) -> bool:
         return _str == _str[::-1]
 
-    # maybe the whole thing is a palindrome
     if is_palindrome(s):
         return s
 
-    substrings: dict[len, list[str]] = {}
-    for i in range(1 + len(s)):
-        for j in range(i, 1 + len(s)):
-            substring = s[i:j]
-            if substring and is_palindrome(substring):
-                substrings.setdefault(len(substring), []).append(substring)
+    window_len = 1
+    palindrome_start = 0
 
-    max_len = max(substrings)
-    return substrings[max_len][0]
+    for i in range(1, len(s)):
+        window_size = i - window_len
+        window_stop = i + 1
+
+        if window_size >= 0 and is_palindrome(s[window_size:window_stop]):
+            palindrome_start = window_size
+            window_len += 1
+
+        elif window_size >= 1 and is_palindrome(s[window_size - 1 : window_stop]):
+            palindrome_start = window_size - 1
+            window_len += 2
+
+    return s[palindrome_start : palindrome_start + window_len]
 
 
 if __name__ == "__main__":
